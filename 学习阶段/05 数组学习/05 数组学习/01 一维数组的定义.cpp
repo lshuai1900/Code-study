d99ecbf07e686f73ead310d7b4023691d7bbdcf3
lshuai1900 <88922428+lshuai1900@users.noreply.github.com>
2025-09-28 18:14:36 +0800

    Update 01 一维数组的定义.cpp

---
+++ 学习阶段/05 数组学习/05 数组学习/01 一维数组的定义.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//数组
+
+	//int arr[5];
+	////给数组赋值
+	////下标是从0开始的
+	//arr[0] = 10;
+	//arr[1] = 20;
+	//arr[2] = 30;
+	//arr[3] = 40;
+	//arr[4] = 50;
+	////访问数组元素
+
+	//cout << arr[0] << endl;
+
+
+	//int arr2[5] = { 1,2,3,4,5 };
+	///*cout << arr2[0] << endl;*/
+
+	//for(int i = 0; i < 5; i++)
+	//{
+	//	cout << arr2[i] << endl;
+	//}
+
+	int arr3[] = { 1,2,3,4,5,6,7,8,9 };
+
+	cout << sizeof(arr3) << endl; //36
+
+
+
+	system("pause");
+	return 0;
+}
