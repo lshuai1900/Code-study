997b0062ef23cb553cfb4b93221653698f4ecdcc
lshuai1900 <sliu040901@foxmail.com>
2025-09-29 16:18:34 +0800

    常规更新

---
+++ 学习阶段/06 函数/06 函数/01 函数的定义.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+//函数的定义
+//返回值类型 函数名(参数列表){函数体}
+
+//int add(int a, int b) 
+//{
+//	int sum = a + b;
+//	return sum;
+//}
+
+
+
+int main1() {
+
+	
+
+	system("pause");
+
+	return 0;
+}
---
+++ 学习阶段/06 函数/06 函数/02 函数的调用.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+//函数的定义
+//返回值类型 函数名(参数列表){函数体}
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	return sum;
+}
+
+
+
+int main() {
+	//main函数的调用add函数
+	int num1 = 10;
+	int num2 = 20;
+
+	int result = add(num1, num2);
+
+	cout << "result = " << result << endl;
+
+	system("pause");
+
+	return 0;
+}
