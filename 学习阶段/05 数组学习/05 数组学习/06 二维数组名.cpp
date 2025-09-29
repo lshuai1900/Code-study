cd8d20fc7013c37dca74cb069c5c0f27ebb9f63a
lshuai1900 <sliu040901@foxmail.com>
2025-09-29 15:31:39 +0800

    20250929

---
+++ 学习阶段/05 数组学习/05 数组学习/06 二维数组名.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main6()
+//{
+//
+//	//int arr[2][3]=
+//	//{ {1,2,3},{4,5,6} };
+//
+//	//cout << "二维数组占用内存空间为：" << sizeof(arr) << endl;
+//	//cout << "二维数组第一行占用内存空间为：" << sizeof(arr[0]) << endl;
+//	//cout << "二维数组每个元素内存空间为：" << sizeof(arr[0][0]) << endl;
+//
+//	//int i = 0;
+//	//int j = 0;
+//	//i = sizeof(arr) / sizeof(arr[0]);
+//	//j = sizeof(arr[0]) / sizeof(arr[0][0]);
+//	//cout << "二维数组行数为：" << i << endl;
+//	//cout << "二维数组列数为：" << j << endl;
+//
+//	//cout << (int)arr << endl;       //输出首行首列元素的地址
+//
+//
+//	//cout << (int)arr[0] << endl;    //输出首行首列元素的地址
+//	//cout << (int)arr[1] << endl;
+//
+//	//cout << (int)&arr[0][0] << endl;
+//
+//	system("pause");
+//	return 0;
+//}
