2b6a8886129c7cf9997508652b0b13fcf1d89606
lshuai1900 <sliu040901@foxmail.com>
2025-10-22 10:44:25 +0800

    常规更新

---
+++ 学习阶段/运算符重载/运算符重载/01 加号运算符重载.cpp
@@ -0,0 +1,81 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//
+//
+//class Person
+//{
+//public:
+//	//成员函数重载
+//	/*Person operator+(Person& p)
+//	{
+//		Person temp;
+//		temp.m_A = this->m_A + p.m_B;
+//		temp.m_B = this->m_B + p.m_B;
+//		return temp;
+//	}*/
+//
+//
+//
+//
+//	int m_A;
+//	int m_B;
+//};
+//
+////全局函数重载
+//Person operator+(Person& p1, Person& p2)
+//{
+//	Person temp;
+//	temp.m_A = p1.m_A + p2.m_A;
+//	temp.m_B = p1.m_B + p2.m_B;
+//	return temp;
+//}
+//
+////函数重载
+//Person operator+(Person& p1, int num)
+//{
+//	Person temp;
+//	temp.m_A = p1.m_A + num;
+//	temp.m_B = p1.m_B + num;
+//	return temp;
+//}
+//
+//void test01()
+//{
+//	Person p1;
+//	p1.m_A = 10;
+//	p1.m_B = 10;
+//
+//	Person p2;
+//	p2.m_A = 10;
+//	p2.m_B = 10;
+//
+//	//成员函数本质：//相当于 Person p3 = p1.operaor+(p2)
+//	//全局函数重载：//相当于 Person p3 = operaor+(p1,p2)
+//	Person p3 = p1 + p2;
+//
+//
+//	//运算符重载也可以发生函数重载
+//
+//	Person p4 = p1 + 100;
+//	
+//
+//	cout << "p3.m_A = " << p3.m_A << endl;
+//	cout << "p3.m_B = " << p3.m_B << endl;
+//
+//	cout << "p4.m_A = " << p4.m_A << endl;
+//	cout << "p4.m_B = " << p4.m_B << endl;
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 学习阶段/运算符重载/运算符重载/03 递增运算符重载.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+
+class MyInteger
+{
+	friend ostream& operator<<(ostream& cout, MyInteger myint);
+public:
+	MyInteger()
+	{
+		m_Num = 0;
+	}
+
+	//重载前置++运算符
+	MyInteger& operator++()
+	{
+		m_Num++;
+		return *this;
+	}
+
+
+
+	//重载后置++运算符
+	MyInteger operator++(int)
+	{
+		MyInteger temp = *this;
+
+		m_Num++;
+
+		return temp;
+	}
+
+private:
+
+
+	int m_Num;
+};
+
+ostream& operator<<(ostream& cout, MyInteger myint)
+{
+	cout << myint.m_Num;
+	return cout;
+}
+
+
+void test01()
+{
+	MyInteger myint;
+
+	cout << ++myint << endl;
+}
+
+void test02()
+{
+	MyInteger myint;
+
+	cout << myint++ << endl;
+	cout << myint << endl;
+}
+
+int main()
+{
+
+	test02();
+
+
+
+	system("pause");
+	return 0;
+}
