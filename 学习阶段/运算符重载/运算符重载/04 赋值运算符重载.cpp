4c319bee2dc55c5222006c1067daff7fd5c4718c
lshuai1900 <sliu040901@foxmail.com>
2025-10-22 18:54:36 +0800

    常规更新

--- 学习阶段/运算符重载/运算符重载/04 赋值运算符重载.cpp
+++ 学习阶段/运算符重载/运算符重载/04 赋值运算符重载.cpp
@@ -1,48 +1,73 @@
-#include<iostream>
-using namespace std;
-
-
-class Person
-{
-public:
-
-	Person(int age)
-	{
-		m_Age = new int(age);
-	}
-
-	~Person()
-	{
-		if (m_Age != NULL)
-		{
-			delete m_Age;
-			m_Age = NULL;
-		}
-	}
-
-	int* m_Age;
-};
-
-void test01()
-{
-	Person p1(18);
-
-	Person p2(20);
-
-	p2 = p1;
-
-	cout << "p1 age = " << *p1.m_Age << endl;
-	cout << "p2 age = " << *p2.m_Age << endl;
-}
-
-
-int main()
-{
-
-	test01();
-
-
-
-	system("pause");
-	return 0;
-}
+//
+//#include<iostream>
+//using namespace std;
+//
+//
+//class Person
+//{
+//public:
+//
+//	Person(int age)
+//	{
+//		m_Age = new int(age);
+//	}
+//
+//	~Person()
+//	{
+//		if (m_Age != NULL)
+//		{
+//			delete m_Age;
+//			m_Age = NULL;
+//		}
+//	}
+//
+//	Person& operator=(Person& p)
+//	{
+//		if (m_Age != NULL)
+//		{
+//			delete m_Age;
+//			m_Age = NULL;
+//		}
+//
+//		//Éî¿½±´
+//		m_Age = new int(*p.m_Age);
+//
+//		return *this;
+//	}
+//
+//	int* m_Age;
+//};
+//
+//void test01()
+//{
+//	Person p1(18);
+//
+//	Person p2(20);
+//
+//	Person p3(30);
+//
+//	p3 = p2 = p1;
+//
+//	cout << "p1 age = " << *p1.m_Age << endl;
+//	cout << "p2 age = " << *p2.m_Age << endl;
+//	cout << "p3 age = " << *p3.m_Age << endl;
+//}
+//
+//
+//int main()
+//{
+//
+//	test01();
+//
+//	/*int a = 10;
+//	int b = 20;
+//	int c = 30;
+//
+//	c = b = a;
+//
+//	cout << "a" << a << endl;
+//	cout << "b" << b << endl;
+//	cout << "c" << c << endl;*/
+//	system("pause");
+//	return 0;
+//}
